bd6fc5a7cd13d929835d2645dfb693d6d92aaf66
samantha-eva <samanthathiebaut@gmail.com>
2024-12-14 15:18:34 +0100

    navbar avec user

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -9,3 +9,9 @@ let shoppingCart = document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick = () =>{
     shoppingCart.classList.toggle('active');
 }
+
+let userLogin = document.querySelector('.user-login');
+
+document.querySelector('#user-btn').onclick = () =>{
+    userLogin.classList.toggle('active');
+}
