a0d30a50569c865497fe14fe1abef05b1ad1082c
samantha-eva <samanthathiebaut@gmail.com>
2025-01-16 05:32:23 +0100

    Recuperer les datas des marques des bonbons

--- src/Controller/BonbonController.php
+++ src/Controller/BonbonController.php
@@ -6,15 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\BonbonsRepository;
+use App\Repository\MarquesRepository;
 class BonbonController extends AbstractController
 
 {
 
     private $bonbonsRepository;
+    private $marquesRepository;
 
-    public function __construct(BonbonsRepository $bonbonsRepository)
+    public function __construct(BonbonsRepository $bonbonsRepository,MarquesRepository $marquesRepository)
     {
         $this->bonbonsRepository = $bonbonsRepository;
+        $this->marquesRepository = $marquesRepository;
     }
 
     #[Route('/boutique', name: 'app_shop')]
@@ -22,10 +25,12 @@ class BonbonController extends AbstractController
     {
 
         $bonbons = $this->bonbonsRepository->findAll();
+        $marques= $this->marquesRepository->findAll();
 
         return $this->render('boutique/index.html.twig', [
             'controller_name' => 'BonbonController',
-            'bonbons'=> $bonbons
+            'bonbons'=> $bonbons,
+            'marques'=> $marques,
         ]);
     }
 }
