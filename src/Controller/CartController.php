42e3f92c8fabdccb5977347e23cb03b8b77a0df7
samantha-eva <samanthathiebaut@gmail.com>
2025-01-26 21:35:47 +0100

    reorganisation du code pour le panier + installation api stripe

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,17 +2,14 @@
 
 namespace App\Controller;
 
-use App\Entity\Bonbons;
-use App\Entity\Commandes;
-use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\CommandeDetails;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Service\CartService;
-use App\Form\CommandeFormType;
+
 
 class CartController extends AbstractController
 {
@@ -25,64 +22,6 @@ class CartController extends AbstractController
        
     }
 
-    #[Route('/checkout', name: 'app_checkout')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $totalPrice = $this->cartService->getTotalPrice($request);
-        $cart = $this->cartService->getCartContents($request);
-        $totalItems = $this->cartService->getTotalItems($request);
-        $user = $this->getUser(); // Récupérer l'utilisateur connecté
-       
-        $form = $this->createForm(CommandeFormType::class, null, [
-            'user' => $user, // Passer l'utilisateur au formulaire
-        ]);
-
-        // Traitement du formulaire
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            //Créer une nouvelle commande
-            $commande = new Commandes();
-            $commande->setUser($user);
-            $commande->setTransporteur($form->get('transporteurs')->getData());
-            $commande->setStatut('à payer');
-            $commande->setPrixTotal($totalPrice);
-            $commande->setCreatedAt(new \DateTime());
-            $commande->setUpdatedAt(new \DateTime());
-
-            // Sauvegarder la commande en base de données
-            $entityManager->persist($commande);
-            $entityManager->flush();
-            
-            foreach( $cart as $produit){
-                $bonbon = $entityManager->getRepository(Bonbons::class)->find($produit['id']);
-
-               
-                $commande_detail  = new CommandeDetails();
-                $commande_detail->setCommande($commande);
-                $commande_detail->setProduit($bonbon);
-                $commande_detail->setQuantite($produit['quantity']);
-                $commande_detail->setPrix($produit['total']);
-
-                $entityManager->persist($commande_detail);
-            }
-
-            $entityManager->flush();
-
-            // Rediriger vers la page de confirmation ou de paiement
-            return $this->redirectToRoute('app_home');
-        }
-
-
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
-            'cart_total_price' => $totalPrice,
-            'cart' => $cart,
-            'cart_total_items' => $totalItems,
-            'form' => $form->createView(),
-        ]);
-    }
 
     #[Route('/add-to-cart', name: 'add_to_cart', methods: ['POST'])]
     public function addToCart(Request $request): JsonResponse
---
+++ src/Controller/PaiementController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Users;
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PaiementController extends AbstractController
+{
+    
+}
