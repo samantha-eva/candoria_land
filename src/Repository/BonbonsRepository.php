7e3b35bf849f3f4fd07b566e8d654ca33a5018ab
samantha-eva <samanthathiebaut@gmail.com>
2025-02-07 00:06:00 +0100

    pagination sur la page boutique

--- src/Repository/BonbonsRepository.php
+++ src/Repository/BonbonsRepository.php
@@ -4,7 +4,9 @@ namespace App\Repository;
 
 use App\Entity\Bonbons;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @extends ServiceEntityRepository<Bonbons>
@@ -21,34 +23,37 @@ class BonbonsRepository extends ServiceEntityRepository
         return $this->findAll();
     }
 
-   public function findBySearchTermAndCategoriesAndMarques($searchTerm, $selectedCategories, $selectedMarques): array
-   {
-
-       $qb = $this->createQueryBuilder('b');
-       
-       if (!empty($searchTerm)) {
-           $qb->where('b.nom LIKE :searchTerm')
+    public function findBySearchTermAndCategoriesAndMarques($searchTerm, $selectedCategories, $selectedMarques, $page = 1, $limit = 8): Paginator
+    {
+        $qb = $this->createQueryBuilder('b');
+    
+        if (!empty($searchTerm)) {
+            $qb->where('b.nom LIKE :searchTerm')
                ->setParameter('searchTerm', '%' . $searchTerm . '%');
-       }
-
-
-       if (!empty($selectedCategories)) {
-           $qb->innerJoin('b.categories', 'c')
+        }
+    
+        if (!empty($selectedCategories)) {
+            $qb->innerJoin('b.categories', 'c')
                ->andWhere('c.id IN (:categories)')
                ->setParameter('categories', $selectedCategories);
-       }
-
-       if (!empty($selectedMarques)) {
-            $qb->innerJoin('b.marque', 'm')
-                ->andWhere('m.id IN (:marques)')
-                ->setParameter('marques', $selectedMarques);
         }
-        $qb->andWhere('b.isPromotion = :isPromotion')
-        ->setParameter('isPromotion', false);
     
-   
-       return $qb->getQuery()->getResult();
-   }
+        if (!empty($selectedMarques)) {
+            $qb->innerJoin('b.marque', 'm')
+               ->andWhere('m.id IN (:marques)')
+               ->setParameter('marques', $selectedMarques);
+        }
+    
+        $qb->andWhere('b.isPromotion = :isPromotion')
+           ->setParameter('isPromotion', false);
+    
+        // Gestion de la pagination
+        $qb->setFirstResult(($page - 1) * $limit) 
+           ->setMaxResults($limit);
+    
+        return new Paginator($qb->getQuery(), true);
+    }
+    
 
    public function findBySearchTermAndCategoriesAndMarquesAndPromotion($searchTerm, $selectedCategories, $selectedMarques): array
    {
