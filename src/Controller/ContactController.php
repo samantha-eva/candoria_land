55bcf4fee7dc8bcd9cf7b5ac148cbc9af785ff76
samantha-eva <samanthathiebaut@gmail.com>
2025-02-09 02:06:42 +0100

    envoie de mail contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,15 +7,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Service\CartService;
 use Symfony\Component\HttpFoundation\Request;
+use App\Service\SendEmailService;
 
 class ContactController extends AbstractController
 {
 
     private $cartService;
+    private SendEmailService $sendEmailService;
 
-    public function __construct( CartService $cartService)
+    public function __construct( CartService $cartService, SendEmailService $sendEmailService)
     {
         $this->cartService = $cartService;
+        $this->sendEmailService = $sendEmailService;
     }
 
 
@@ -26,6 +29,30 @@ class ContactController extends AbstractController
          // Récupérer les données du panier
          $totalItems = $this->cartService->getTotalItems($request);
          $totalPrice = $this->cartService->getTotalPrice($request);
+        
+
+
+         if ($request->isMethod('POST')) {
+            $email = $request->request->get('email');
+            $subject = $request->request->get('subject');
+            $message = $request->request->get('message');
+
+            if ($email && $subject && $message) {
+                // Envoi de l'email
+                $this->sendEmailService->send(
+                    'no-reply@example.com', 
+                    $email, 
+                    'Contact - candoria ', 
+                    'contact',
+                    compact('message')
+                );
+
+                $this->addFlash('success', 'Votre message a bien été envoyé.');
+                return $this->redirectToRoute('app_contact');
+            }
+
+            $this->addFlash('error', 'Tous les champs sont obligatoires.');
+        }
   
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
@@ -33,4 +60,5 @@ class ContactController extends AbstractController
             'cart_total_price' => $totalPrice,
         ]);
     }
+    
 }
